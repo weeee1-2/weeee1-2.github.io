af45abd914c17950e8cabce3e88df72325123e85
drowsyindra <3220103865@zju.edu.cn>
2024-08-24 19:30:25 +0800

    feat: debug2_404

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeKatex from "rehype-katex"
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://maindraster.github.io',
+	base: "/",
 	integrations: [
 		starlight({
 			plugins: [starlightImageZoom()],
