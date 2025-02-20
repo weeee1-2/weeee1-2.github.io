0fb69fb2f33b070ad3462f725ad1897159b27060
maindraster <tangkunyi154@gmail.com>
2025-02-20 08:25:43 +0800

    test:Astro 5.0-new big change for cover

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import contextCursor from "../libs/context-cursor/contextCursor/index.ts";
+import contextCursor from "../libs/context-cursor/contextCursor";
 
 var mq = window.matchMedia("(min-width: 640px)");
 if (mq.matches) {
